083c7b6b0ec5f609be684f8bd208406a14cf1379
Alden Kruse <57150710+1alden@users.noreply.github.com>
2022-11-04 13:30:09 -0500

    Add files via upload

---
+++ changeBG.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeBG : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    public Sprite Day;
+    public Sprite Night;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        if (Random.Range(0, 3) == 0)
+        {
+            sr.sprite = Night;
+        }
+        else
+        {
+            sr.sprite = Day;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
